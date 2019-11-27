0d4429cb245096fc8face85862ad42e27ce72028
Anas Bellouzi <anas.bellouzi@students.makeschool.com>
2019-11-26 22:06:47 -0800

    added images and results view

--- brainGame/ViewController.swift
+++ brainGame/ViewController.swift
@@ -9,65 +9,112 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
+    @IBOutlet var sceneView: UIView!
     @IBOutlet weak var meaningLabel: UILabel!
+    
     @IBOutlet weak var colorTextLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    @IBOutlet weak var upsideImageView: UIImageView!
     
+    @IBOutlet weak var upImageView: UIImageView!
     var score: Int = 0
+    var highScore: Int = 0
     
-    let colors: [String:UIColor] = ["red": UIColor.red, "green": UIColor.green, "yellow": UIColor.yellow, "purple": UIColor.purple, "orange": UIColor.orange, "blue": UIColor.blue, "brown": UIColor.brown, "gray": UIColor.gray]
+    let colors: [String:UIColor] = ["red": .red,
+                                    "green": .green,
+                                    "yellow": .yellow,
+                                    "purple": .purple,
+                                    "orange": .orange,
+                                    "blue": .blue,
+                                    "brown": .brown,
+                                    "gray": .gray]
 
     var currentColor: UIColor = UIColor.white
     var currentText: String = ""
 
-    
+    // loads the view
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setupScene()
         refreshGame()
     }
     
+    // pass high score to result view
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showEndGame" {
+            if let resultViewController = segue.destination as? resultViewController {
+                    resultViewController.highScore = self.highScore
+            }
+        }
+    }
+    
+    // style for user view
+    func setupScene() {
+        meaningLabel.layer.masksToBounds = true
+        colorTextLabel.layer.masksToBounds = true
+        meaningLabel.layer.cornerRadius = 20
+        colorTextLabel.layer.cornerRadius = 20
+        yesButton.layer.cornerRadius = 5
+        noButton.layer.cornerRadius = 5
+        yesButton.backgroundColor = UIColor.init(displayP3Red: 30, green: 30, blue: 30, alpha: 0.4)
+        noButton.backgroundColor = UIColor.init(displayP3Red: 30, green: 30, blue: 30, alpha: 0.4)
+        sceneView.backgroundColor = UIColor(patternImage: UIImage(named: "viewBg.jpg")!)
+        
+        upsideImageView.image = upsideImageView.image?.withRenderingMode(.alwaysTemplate)
+        upImageView.image = upImageView.image?.withRenderingMode(.alwaysTemplate)
+        
+        
+    }
+
+    // controls current game attributes
     func refreshGame(){
         currentText = colors.randomElement()!.key
         currentColor = colors.randomElement()!.value
-    
+        let randomColor = colors.randomElement()?.key
         
+        meaningLabel.text = randomColor
         colorTextLabel.text = currentText
         meaningLabel.textColor = currentColor
-        meaningLabel.text = colors.randomElement()?.key
+        
+        upsideImageView.tintColor = colors.randomElement()!.value
+        upImageView.tintColor = colors[currentText]
         
         if (score <= -1) {
-            scoreLabel.text = "You Lose!"
+            score = 0
+            scoreLabel.text = String(highScore)
+            performSegue(withIdentifier: "showEndGame", sender: self)
+            
         } else {
             scoreLabel.text = String(score)
         }
-        
     }
     
-    
+    // yes button
     @IBAction func yesActionButtton(_ sender: Any) {
         if(colors[currentText] == currentColor) {
-            score += 1
+            score += 500
+            highScore += 500
         }
         else {
-            score -= 1
+            score -= 500
         }
         refreshGame()
         
     }
     
+    // no button
     @IBAction func noActionButton(_ sender: Any) {
-        
         if(colors[currentText] != currentColor) {
-            score += 1
+            score += 500
+            highScore += 500
         }
         else {
-            score -= 1
+            score -= 500
         }
         refreshGame()
-        
     }
 }
 
---
+++ brainGame/resultViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  brainGame
+//
+//  Created by Anas on 11/25/19.
+//  Copyright © 2019 Anas. All rights reserved.
+//
+
+import UIKit
+
+class resultViewController: UIViewController {
+   
+    @IBOutlet var sceneView: UIView!
+    @IBOutlet weak var playAgainButton: UIButton!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    var highScore: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        setupScene()
+        
+    }
+    
+    func setupScene() {
+        playAgainButton.layer.masksToBounds = true
+        playAgainButton.layer.cornerRadius = 20
+        
+        playAgainButton.backgroundColor = UIColor.init(displayP3Red: 30, green: 30, blue: 30, alpha: 0.4)
+        sceneView.backgroundColor = UIColor(patternImage: UIImage(named: "viewBg.jpg")!)
+        
+        scoreLabel.text = String(highScore)
+    }
+    
+    
+ 
+}
+
